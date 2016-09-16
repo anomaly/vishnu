1cbb12bc67ac36f6d6dc93b0b69ef681f6193a55
Bradley Mclain <brad@mclain.com.au>
2016-09-16 14:39:11 +1000

    adds version init

--- vishnu/__init__.py
+++ vishnu/__init__.py
@@ -5,6 +5,9 @@ from __future__ import absolute_import
 
 import threading
 
+__version_info__ = (1, 0, 2)
+__version__ = '.'.join(str(v) for v in __version_info__)
+
 _thread_local = threading.local()  # pylint: disable=C0103
 _thread_local.session = None
 
