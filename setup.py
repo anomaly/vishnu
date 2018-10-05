05e39ad61972af5a4ebc5b8619bf954452a9edf1
Bradley Mclain <brad@mclain.com.au>
2018-10-05 16:49:40 +1000

    excludes tests from setup, adds unittest to tox

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
     platforms=['any'],
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     install_requires=[
         "future",
